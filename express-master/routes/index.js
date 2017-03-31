729c79845c84b9625b44ef76bdb6e1ba31a2f532
htks <kbsd@hotmail.com>
2017-03-30 19:35:31 -0500

    it all works!

--- express-master/routes/index.js
+++ express-master/routes/index.js
@@ -1,67 +1,127 @@
 var express = require('express');
 var router = express.Router();
-var ObjectID = require('mongodb').ObjectID
+var Task = require('../models/task.js');
+
+
 /* GET home page, a list of incomplete tasks . */
 router.get('/', function(req, res, next) {
 
-    req.task_col.find({completed:false}).toArray(function(err, tasks){
+    Task.find({completed:false}, function(err, tasks){
         if (err) {
             return next(err);
         }
         res.render('index', { title: 'TODO list' , tasks: tasks });
     });
 });
+
+
+
+/* GET all completed tasks. */
 router.get('/completed', function(req, res, next){
-    req.task_col.find({completed:true}).toArray(function(err, tasks){
+
+    Task.find({completed:true}, function(err, tasks){
         if (err) {
             return next(err);
         }
-        res.render('tasks_completed', { title: 'Completed tasks' , tasks: tasks });
+        res.render('tasks_completed', { title: 'TODO list' , tasks: tasks });
+    });
+
+});
+
+
+/* Mark all tasks as done. */
+router.post('/alldone', function(req, res, next){
+
+    Task.update( {completed:false}, {completed:true}, {multi:true}, function(err){
+
+        if (err) {
+            return next(err);
+        }
+
+        req.flash('info', 'All tasks are done!');
+        return res.redirect('/')
+
     });
 });
+
+
+
+
+/* Show details of one task */
+router.get('/task/:id', function(req, res, next){
+
+    Task.findById(req.params.id, function(err, task){
+        if (err) {
+            return next(err);
+        }
+        return res.render('task_detail', {task:task})
+    })
+});
+
+
 /* POST Add new task, then redirect to task list */
 router.post('/add', function(req, res, next){
 
     if (!req.body || !req.body.text) {
-        //error message for user
         req.flash('error', 'Please enter some text');
         res.redirect('/');
     }
 
     else {
         // Save new task with text provided, and completed = false
-        var task = { text : req.body.text, completed: false};
+        var task = Task({ text : req.body.text, completed: false});
 
-        req.task_col.insertOne(task, function(err, task) {
+        task.save(function(err) {
             if (err) {
                 return next(err);
             }
-            res.redirect('/')
-        })
+            return res.redirect('/')
+        });
     }
 
 });
-//adds done button
+
+
+/* Mark a task as done. Task _id should be provided as req.body parameter */
 router.post('/done', function(req, res, next){
-//convert object string to object object
-    req.task_col.updateOne(
-        { _id : ObjectID(req.body._id) },
-        {$set : { completed : true }},
-        function(err, result) {
+
+    var id = req.body._id;
+    Task.findByIdAndUpdate(id, {completed:true}, function(err, task){
 
         if (err) {
-            return next(err);    // For database errors, 500 error
+            return next(err);
         }
 
-        if (result.result.n == 0) {
+        if (!task) {
             var req_err = new Error('Task not found');
             req_err.status = 404;
             return next(req_err);     // Task not found error
         }
-        req.flash('info', 'Marked as completed');
+
+        req.flash('info', 'Task marked as done');
         return res.redirect('/')
-    })
+
     });
+
+});
+
+
+/* Delete a task. Task _id is in req.body */
+router.post('/delete', function(req, res,next){
+
+    var id = req.body._id;
+
+    Task.findByIdAndRemove(id, function(err){
+
+        if (err) {
+            return next(err);    // For database errors
+        }
+
+        req.flash('info', 'Deleted');
+        return res.redirect('/')
+
+    })
+});
 
 
 module.exports = router;
